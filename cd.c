e7e507ef8fef598803f2ff5d4ecc51196d1c7eb9
teandrian <tevannd@gmail.com>
2025-05-08 15:36:48 +0000

    rm

--- cd.c
+++ cd.c
@@ -1,27 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   echo.c                                             :+:      :+:    :+:   */
+/*   cd .c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: teandria < teandria@student.42nice.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/20 02:13:25 by teandria          #+#    #+#             */
-/*   Updated: 2025/04/12 01:52:36 by teandria         ###   ########.fr       */
+/*   Created: 2025/03/29 02:13:25 by teandria          #+#    #+#             */
+/*   Updated: 2025/04/14 13:55:58 by teandria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	ft_echo(char **args, int n)
+int ft_cd(char **args, t_env **env)
 {
-	while (*args)
-	{
-		ft_putstr_fd(*args, 1);
-		args++;
-		if (*args)
-			write (1, " ", 1);
-	}
-	if (!n)
-		write(1, "\n", 1);
-	return (0);
+    
 }
